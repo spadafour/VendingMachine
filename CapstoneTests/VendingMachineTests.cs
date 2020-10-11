03d68a2d26c61bbd23e9042e1d1e1f81ada2e709
Angela Zollman <amnagem@gmail.com>
2020-10-10 21:57:34 -0400

    Tests for MakeChange and FeedMoney in VendingMachine

--- CapstoneTests/VendingMachineTests.cs
+++ CapstoneTests/VendingMachineTests.cs
@@ -11,7 +11,7 @@ namespace CapstoneTests
     {
         //Testing Balance
         [TestMethod]
-        public void Balance()
+        public void BalanceAtStart()
         {
             VendingMachine vendingMachine = new VendingMachine();
             decimal expected = 0;
@@ -19,17 +19,57 @@ namespace CapstoneTests
 
             Assert.AreEqual(expected, result);
         }
+        //TODO: Test for VendSelectedItem
+
+        //FeedMoney test
+        [TestMethod]
+        public void FeedMoney()
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+            vendingMachine.FeedMoney(10);
+            decimal expected = 10;
+            decimal result = vendingMachine.Balance;
+
+            Assert.AreEqual(expected, result);
+
+        }
+
         //Testing MakeChange Dictionary, but pretty sure this isn't right even though it passes...
         [TestMethod]
         public void MakeChange()
         {
-            VendingMachine vendingMachine = new Vendingmachine();
-            Dictionary<string, int> expected = vendingMachine.MakeChange();
+            VendingMachine vendingMachine = new VendingMachine();
+            vendingMachine.FeedMoney(10);
+            Dictionary<string, int> expected = new Dictionary<string, int>
+            {
+                { "Quarters", 40 }, { "Dimes", 0 }, { "Nickels", 0 }, { "Pennies", 0 }
+            };
             Dictionary<string, int> result = vendingMachine.MakeChange();
 
             CollectionAssert.AreEqual(expected, result);
 
-            //Testing MakeChange Dictionary exceptions, but I actually don't know how to go about doing this for a dictionary...
+            vendingMachine = new VendingMachine();
+            vendingMachine.FeedMoney(11.61M);
+            expected = new Dictionary<string, int>
+            {
+                { "Quarters", 46 }, { "Dimes", 1 }, { "Nickels", 0 }, { "Pennies", 1 }
+            };
+            result = vendingMachine.MakeChange();
+
+            CollectionAssert.AreEqual(expected, result);
+
+            vendingMachine = new VendingMachine();
+            vendingMachine.FeedMoney(1.05M);
+            expected = new Dictionary<string, int>
+            {
+                { "Quarters", 4 }, { "Dimes", 0 }, { "Nickels", 1 }, { "Pennies", 0 }
+            };
+            result = vendingMachine.MakeChange();
+
+            CollectionAssert.AreEqual(expected, result);
+
+            //TODO: Testing MakeChange Dictionary exceptions below, but I actually don't know how to go about doing this for a dictionary...
+
         }
     }
 }
